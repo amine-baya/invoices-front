bef4220a7745ab270120b7165c9a4d47b1f4297f
baya <abaya6054@gmail.com>
2023-03-14 23:10:58 +0100

    generate  different file names

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,9 @@ const App = () => {
     axios.post('https://invoices-back.onrender.com/create-pdf', data)
       .then(() => axios.get('https://invoices-back.onrender.com/fetch-pdf', { responseType: 'blob' }))
       .then((res) => {
+        const timestamp = new Date().getTime();
         const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
-        saveAs(pdfBlob, 'newPdf.pdf');
+        saveAs(pdfBlob, `newPdf-${timestamp}.pdf`);
       })
   }
 
